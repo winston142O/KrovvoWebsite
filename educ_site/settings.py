863b398cb82abe0665a68566f0ceab4fba8da3c5
Winston Pichardo <winston1420@hotmail.com>
2022-10-27 19:20:34 -0500

    Update settings.py
    removed secret key

--- educ_site/settings.py
+++ educ_site/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t9mn=^5_fi^q63dioylq@=^1wrf+(v@&h@km7mg(1*b2!)rqao'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
